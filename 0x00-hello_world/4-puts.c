7f2147d7ab5a409a50f5af3517e9429e4edc3ed3
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-08 15:30:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ * main - the entry point
+ *
+ * Return: return the integer 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
