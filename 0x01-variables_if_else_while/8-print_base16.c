f4ad9389280cf148315a47f483e62849e497fb76
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-09 16:21:47 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,9 +13,11 @@ int main(void)
 
 	for (digit = '0'; digit <= '9'; digit++)
 	{
- 		putchar(digit);
+		putchar(digit);
 		if (digit == '9')
-		{											digit = 'a';									for (; digit <= 'f'; digit++)
+		{								
+			digit = 'a';						
+			for (; digit <= 'f'; digit++)
 			{											putchar(digit);
 			} 										break;									}
 	}
