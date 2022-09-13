e81d684c713ea1c29a34f0a1f509cd8172927696
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-13 05:47:27 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Description: prints a-z in lowercase
+ * Return - always 0
+*/
+int main(i)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
+
