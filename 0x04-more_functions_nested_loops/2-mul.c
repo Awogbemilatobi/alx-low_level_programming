09b394d485c59089d0be2f553b5aa613f55f6683
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-15 13:07:38 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
- * mul - multiplies 2 integers
- *
- * Return: returns an integer
-*/
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
 int mul(int a, int b)
 {
-	int a;
-	int b;
-	mul = a*b
+	int c;
 
+	c = a * b;
+
+	return (c);
+}
