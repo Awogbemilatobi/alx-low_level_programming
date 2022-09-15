1b346f67343cb7a8fa12279a8757f3f315cd32c3
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-15 04:53:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 1 success
+*/
+int _isupper(int c)
+{
+	int c;
+
+	if c >= 65 && c <= 90;
+	{
+		return 1;
+	}
+
+	else
+	{	
+		return 0
+	}
+} 
