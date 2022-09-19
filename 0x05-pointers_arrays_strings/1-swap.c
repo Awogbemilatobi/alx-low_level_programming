8915da76995b881213a50084e5a89073990d44da
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-19 08:27:40 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_in - swaps two integers
+ * @n: integer
+ * Return: 0 as success
+*/
+void swap_int(int *a, int *b)
+{
+	int c = a;
+	int *a = *b;
+	int *b = c;
+}
+
