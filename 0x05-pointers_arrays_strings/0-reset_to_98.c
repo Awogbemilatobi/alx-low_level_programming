7c8274f0314deca8a7ad621c187ed6d6d0e4588a
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-19 08:15:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - reset integer to 98
- *@n: integer pointer
+ * reset_to_98 - reset integer to 98
+ * @n: integer pointer
  * Return: 0 as success
 */
 void reset_to_98(int *n)
