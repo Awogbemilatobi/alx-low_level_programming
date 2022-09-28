c4301181a218b391eed223297513c75402e934c3
Awogbemilatobi <awogbemilatobi@gmail.com>
2022-09-28 08:01:09 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,11 +3,8 @@
 
 /**
  * _puts_recursion - prints string
- * @s: string name
+ * @s: string
 */
 void _puts_recursion(char *s)
-{
-	char stri[];
-
-	printf("%s\n", stri[]);
-}
+{ 
+	printf("%s\n", s);                                                      }                           	
